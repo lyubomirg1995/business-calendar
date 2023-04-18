2576579d149d71396802daf2f6647479bf52f1f9
lyubomirg1995 <lyubomirg17@gmail.com>
2023-04-18 15:16:57 -0400

    added READMe and comments

--- README.md
+++ README.md
@@ -2,6 +2,28 @@
 
 # Description
 
-Tracks business hour events. Under construction
+ The average person goes throughout life without keeping a schedule organized with their hour-by-hour activities. This leads to an unproductive day, as many important tasks are left unfinished due to procrastination. By creating a simple calendar application that tracks the hourly activities of a person's typical 9-5 job, one can organize their time more effectively and increase their work output. You will no longer have to carry all of your responsibilities in your mind and guess what comes next. Having made this program, I learned basic principles of localStorage, jQuery, DOM traversal, event delegation, and parts of the DayJS library. 
+
+ # Usage
+![alt text](./images/business-calendar.png)
+
+ 1. Click the textarea
+ 2. Write an event
+ 3. Click the save button icon to the right
+ 4. All your events should remain, even after refreshing the page.
+ 5. To delete an event, click on the textarea, erase the contents, then click the save button.
+
+ The application can be accessed here: https://lyubomirg1995.github.io/business-calendar/
+
+# Credits
+
+1. Dynamically changing textbox colors, lines 19-32: Kathyrn Young (Student), Sean McCarthy (Teacher's Assistant), Joshua Bohde (Instructor ) -- Office Hours
+2. Displaying today's time, lines 45-47: https://day.js.org/docs/en/parse/now
+3. Saving textarea to localStorage, lines 7-14: GTP-4 Tutor Prompts (no direct code solutions), Kathyrn Young (lines 10-11) -- Office Hours
+4. Retrieving + persistence of localStorage information, lines 36-41: AskBCS Learning Assistant, dgaither
+
+
+
+
 
 
--- script.js
+++ script.js
@@ -1,14 +1,7 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
 
+$(function () {
+    
+// second argument targets the button,then text context is stored in localstorage
     $('.time-block').on('click', '.saveBtn', function(event) {
         event.preventDefault();
 
@@ -16,17 +9,11 @@ $(function () {
         var timeBlockText = $(this).siblings('.description').val();
 
         localStorage.setItem(timeBlockId, JSON.stringify(timeBlockText));
-
 })
   
   
 
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-
+// each text box has its color conditionally changed according to the current time compared to business hours
     $('.time-block').each(function() {
       var businessHour = parseInt($(this).attr("id").split("-")[1]);
       var currentTime = dayjs().hour();
@@ -41,19 +28,17 @@ $(function () {
       
  })
     
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
     
+    // text-content is retrieved from localStorage and persists after being saved
     $('.time-block').each(function() {
-        var timeBlockId = $(this).parent().attr('id')
+        var timeBlockId = $(this).attr('id');
         var textString = localStorage.getItem(timeBlockId);
         var text = JSON.parse(textString)
         $(this).children('.description').val(text);
     })
 
 
-
+      //current month and day is displayed at the top
        var currentDate = dayjs(); 
       $('#currentDay').text(currentDate.format('dddd, MMMM D YYYY'));
   });
